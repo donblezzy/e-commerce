99290583035a0d6f7b822c420ab4c7e27534b02d
donblezzy <dodusina@yahoo.com>
2025-09-29 16:22:51 +0100

    k

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -4,13 +4,10 @@ export const connectDatabase = () => {
 
     let MONGO_URI =""
 
-    if(process.env.NODE_ENV === "DEVELOPMENT")  MONGO_URI = process.env.MONGO_LOCAL_URI
+    // if(process.env.NODE_ENV === "DEVELOPMENT")  MONGO_URI = process.env.MONGO_LOCAL_URI
     if(process.env.NODE_ENV === "PRODUCTION")  MONGO_URI = process.env.MONGO_URI
  
-    mongoose.connect(MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-   }).then((con) => {
+    mongoose.connect(MONGO_URI).then((con) => {
         console.log(`MongoDB Database connected with HOST: ${con?.connection?.host}`);
     })
 }
